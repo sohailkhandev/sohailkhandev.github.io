22159cfed692e59248cb57b8460bb072fd1f95de
Sohail Khan <iiebest03@gmail.com>
2021-10-11 17:15:45 +0500

    update

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// DATE PICKER
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+
+// MULTI SELECT
+$('.demo').dropdown({
+  multipleMode:'label'
+});
+  
